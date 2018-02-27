a6a8f11b589858eb4afb8689249a15e7073e84de
Andrew K DeCotiis-Mauro <andrewkmauro@gmail.com>
2018-02-27 12:07:57 -0500

    First commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='physplt',
+      version='0.1',
+      description='A collection of functions to obscure the matplotlib code needed to plot standard physics graphs',
+      url='https://github.com/kaumaron/physplt',
+      author='Andrew DeCotiis-Mauro',
+      author_email='andrew.decotiis.mauro@gmail.com',
+      license='GPL-3.0+',
+      packages=['physplt'],
+      zip_safe=False)
